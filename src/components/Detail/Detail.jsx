0de16c4499e16d258e623cdc4a051804c7540460
CamilinLopez <camilollc98@gmail.com>
2023-03-26 12:39:36 -0500

    avance detail page

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,13 +1,61 @@
+import axiosInstance from "../../configAxios";
 import style from "./detail.module.css";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
 
 function Detail() {
+    const [detail, setDetail] = useState({});
     const { DetailId } = useParams();
 
+
+    useEffect(() => {
+        console.log("mama");
+        const getDetail = async () => {
+            const data = (await axiosInstance.get(`/recipes/${DetailId}`)).data;
+            setDetail(data);
+        }
+        getDetail();
+    }, [DetailId])
+
+    const highlightWords = (text) => {
+        return text.replace(/<b>(.*?)<\/b>/g, '<strong>$1</strong>');
+    }
+
+
     return (
-        <div className={style.principal} >
-            <h5>{DetailId}</h5>
-        </div>
+        <>
+            {
+                Object.keys(detail).length === 0
+                    ?
+                    <h5>Loading...</h5>
+                    :
+                    <div className={style.princial} >
+
+                        <div className={style.div1} >
+
+                            <div className={style.imgTitle} >
+                                <h6>{detail.title}</h6>
+                                <img src={detail.image} alt={detail.id} />
+                                
+                                <div className={style.info} >
+                                    <p>ID: {detail.id}</p>
+                                    <p className={style.hs} >healthScore: {detail.healthScore}</p>
+                                </div>
+                            </div>
+
+                            <div className={style.summary} >
+                                <h6>summary</h6>
+                                <p dangerouslySetInnerHTML={{ __html: highlightWords(detail.summary) }} />
+                            </div>
+
+                        </div>
+                        <hr />
+
+                    </div>
+            }
+        </>
     )
 }
 
