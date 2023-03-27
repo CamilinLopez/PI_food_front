8bf0436f15e4594b6c897feded3268e5045f00ef
CamilinLopez <camilollc98@gmail.com>
2023-03-27 15:08:59 -0500

    diseño form terminado

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -35,6 +35,10 @@ const From = () => {
     const [diet, setDiet] = useState({
         name: ""
     })
+    const [time, setTime] = useState({
+        number: "",
+        unit: "minutes"
+    })
 
 
     const handleOnChange = (e) => {
@@ -70,6 +74,13 @@ const From = () => {
         setDiet(e.target.value);
     }
 
+    const handleOnChange5 = (e) => {
+        setTime({
+            ...time,
+            [e.target.name]: e.target.value
+        })
+    }
+
     const submit = (e) => {
         e.preventDefault();
         console.log(receta);
@@ -77,42 +88,76 @@ const From = () => {
 
     return (
         <div className={style.principal} >
-            <h4 className={style.title}>Crea tu receta</h4>
-
+            <h4>Crea tu receta</h4>
             <div className={style.contenedor} >
-                <form className={style.form} onSubmit={submit} >
 
-                    <div className={style.inputs1} >
+                <div className={style.fracment1} >
+                    <div className={style.in1} >
                         <input type="text" name="title" value={receta.title} onChange={handleOnChange} placeholder="nombre de la receta" />
+                        <samp>data</samp>
+                    </div>
+                    <div className={style.in1} >
                         <input type="text" name="image" value={receta.image} onChange={handleOnChange} placeholder="agrega una imagen" />
+                        <samp>data</samp>
+                    </div>
+                    <div className={style.in1} >
                         <input type="number" name="healthScore" value={receta.healthScore} onChange={handleOnChange} placeholder="puntaje" />
+                        <samp>data</samp>
                     </div>
+                </div>
 
-                    <div className={style.inputs2} >
+                <div className={style.fracment2} >
+                    <div className={style.in1} >
                         <textarea name="summary" value={receta.summary} onChange={handleOnChange} placeholder="resumen de tu receta" rows="10" />
+                        <samp>error</samp>
                     </div>
+                </div>
 
-                    <div className={style.inputs3} >
+                <div className={style.fracment1} >
+                    <div className={style.in1} >
                         <input type="text" name="step" value={paso.step} onChange={handleOnChange1} placeholder="nombre del paso " />
-                        <div className={style.addingredient} >
+                        <samp>error</samp>
+                    </div>
+                    <div className={style.in1} >
+                        <div className={style.inputmessageingredient} >
                             <input type="text" name="name" value={ingredient.name} onChange={handleOnChange2} placeholder="nombre del ingrediente" />
-                            <button >add</button>
+                            <button>add</button>
                         </div>
-                        <div className={style.equipment} >
+                        <samp>error</samp>
+
+                    </div>
+                    <div className={style.in1} >
+                        <div className={style.intputmessageequipment} >
                             <input type="text" name="name" value={equipment.name} onChange={handleOnChange3} placeholder="nombre del utencilio" />
                             <button>add</button>
                         </div>
-                        <button>add step</button>
+                        <samp>error</samp>
                     </div>
+                    <div className={style.in1} >
+                        <div className={style.addTime} >
+                            <input type="number" name="number" value={receta.healthScore} onChange={handleOnChange5} placeholder="tiempo de coccion" />
+                            <button>add</button>
+                        </div>
+                        <samp>error</samp>
+                    </div>
+                    <button>add step</button>
+                </div>
 
-                    <div className={style.inputs4} >
+
+                <div className={style.fracment1} >
+                    <div className={style.in1}>
                         <input type="text" name="name" value={diet.name} onChange={handleOnChange4} placeholder="nombre de la dieta" />
+                        <samp>error</samp>
                         <p>Mostrar mas</p>
-                        <button>add</button>
                     </div>
-                </form>
+                    <button>add</button>
+                </div>
+
+
             </div>
-            <button className={style.crear} >crear</button>
+            <div className={style.crear} >
+                <button onClick={submit} >crear</button>
+            </div>
         </div>
     )
 }
